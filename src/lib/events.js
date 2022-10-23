9616a4f7bc63968c9fd59768502fb4b99e4d9a50
zuedev <zuedev@gmail.com>
2022-10-23 08:09:33 +0100

    fix event pulling

--- src/lib/events.js
+++ src/lib/events.js
@@ -1,6 +1,6 @@
 import { putItem } from "./dynamodb.js";
 
-export default async function LogEvent(data) {
+export async function LogEvent(data) {
   await putItem("steppenwauwau-events", {
     Epoch: `${Date.now().toString()}-${Math.random().toString().slice(2, 6)}`,
     Data: data,
