ccb91c7d7d10f5e08668d224be28947e55f8bad8
zuedev <zuedev@gmail.com>
2022-10-23 03:56:37 +0100

    commands should register

--- src/scripts/register-commands.js
+++ src/scripts/register-commands.js
@@ -13,21 +13,15 @@ const rest = new REST({ version: "10" }).setToken(
 );
 
 (async () => {
-  try {
-    console.log("Started refreshing application (/) commands.");
+  await rest.put(
+    Routes.applicationGuildCommands(
+      process.env.DISCORD_APPLICATION_ID,
+      process.env.DISCORD_GUILD_ID
+    ),
+    {
+      body: commands,
+    }
+  );
 
-    await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.DISCORD_APPLICATION_ID,
-        process.env.DISCORD_GUILD_ID
-      ),
-      {
-        body: commands,
-      }
-    );
-
-    console.log("Successfully reloaded application (/) commands.");
-  } catch (error) {
-    console.error(error);
-  }
+  console.log("Successfully reloaded application (/) commands.");
 })();
